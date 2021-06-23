e1638ac88a8a835bba73c602821eaf98c73ca0f2
ohfreeya <student40807@gmail.com>
2021-06-23 16:17:27 +0800

    setDB & model

---
+++ app/Platform.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Platform extends Model
+{
+    //
+    protected $fillable=[
+        'name','ISBN','author',
+        'price',
+    ];
+
+}
