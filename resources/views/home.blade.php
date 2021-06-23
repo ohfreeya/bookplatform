5ea7fe89b5c353a40babd4b05d538a651c7accb7
ohfreeya <student40807@gmail.com>
2021-06-23 17:15:44 +0800

    修改標題名稱

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">會員管理系統</div>
+                <div class="card-header">Platform</div>
 
                 <div class="card-body">
                     @if (session('status'))
