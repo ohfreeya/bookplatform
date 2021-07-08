8812b138624461f08dcb6206b1cc898cd59ff708
ohfreeya <student40807@gmail.com>
2021-07-08 15:15:03 +0800

    新增要求欄位

--- resources/views/add-Book.blade.php
+++ resources/views/add-Book.blade.php
@@ -13,9 +13,9 @@
     </div>
     @endif
     <hr>
-    <form action="{{route('add.Book.data')}}" class="was-validated" method="POST">
+    <form action="{{ route('add.Book.data') }}" class="was-validated" method="POST">
         @csrf
-        <input type="hidden" name="id" value="{{Auth::user()->id}}">
+        <input type="hidden" name="seller_id" value="{{Auth::user()->id}}">
         <div class="form-group">
             <label for="uname">出售者:(不可修改) <label>
                     <input type="text" class="form-control" id="seller" name="seller" value="{{Auth::user()->name}}" readonly>
@@ -48,6 +48,41 @@
         <div class="form-group">
             <label for="price">期望金額: <b class="text-danger">(請輸入金額)</b></label>
             <input type="text" class="form-control" id="price" placeholder="請輸入金額" name="price" required>
+            <div class="valid-feedback">完成填寫</div>
+            <div class="invalid-feedback">請填寫此欄位</div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label for="publisher">出版商: <b class="text-danger">(請輸入出版商)</b></label>
+            <input type="text" class="form-control" id="publisher" placeholder="請輸入出版商" name="publisher" required>
+            <div class="valid-feedback">完成填寫</div>
+            <div class="invalid-feedback">請填寫此欄位</div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label for="publishDate">出版日期: <b class="text-danger">(請輸入出版日期)</b></label>
+            <input type="text" class="form-control" id="publishDate" placeholder="請輸入出版日期" name="publishDate" required>
+            <div class="valid-feedback">完成填寫</div>
+            <div class="invalid-feedback">請填寫此欄位</div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label for="edition">再版: <b class="text-danger">(請輸入再版)</b></label>
+            <input type="text" class="form-control" id="edition" placeholder="請輸入再版" name="edition" required>
+            <div class="valid-feedback">完成填寫</div>
+            <div class="invalid-feedback">請填寫此欄位</div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label for="language">語言: <b class="text-danger">(請輸入語言)</b></label>
+            <input type="text" class="form-control" id="language" placeholder="請輸入語言" name="language" required>
+            <div class="valid-feedback">完成填寫</div>
+            <div class="invalid-feedback">請填寫此欄位</div>
+        </div>
+        <hr>
+        <div class="form-group">
+            <label for="bookStatus">書況: <b class="text-danger">(請輸入書況)</b></label>
+            <input type="text" class="form-control" id="bookStatus" placeholder="請輸入書況" name="bookStatus" required>
             <div class="valid-feedback">完成填寫</div>
             <div class="invalid-feedback">請填寫此欄位</div>
         </div>
