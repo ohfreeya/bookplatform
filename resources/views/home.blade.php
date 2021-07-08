9ca86f17706e1a34a7589a56be4e23684a821544
ohfreeya <student40807@gmail.com>
2021-07-08 15:14:45 +0800

    首頁畫面調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,23 +13,38 @@
 </div>
 <hr>
 @if($book_info->isEmpty())
-<h1>查無此資料</h1>
+<h1>暫無資料</h1>
 <a href="{{route('home')}}">返回首頁</a>
+<a href="{{route('add.Book')}}">
+    <div id="add-block">
+        <svg xmlns="http://www.w3.org/2000/svg" width="186" height="170.8" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        </svg>
+    </div>
+</a>
 @elseif($book_info)
-<div class="list_page">
+<div id="list_page">
     @foreach($book_info as $data)
     <div class="card" id="platform-card" style="width: 18rem;">
         <img class="card-img-top" src="..." alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">{{$data->name}}</h5>
-            <p class="card-text">Some</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">ISBN : {{$data->ISBN}}</h5>
+            <h5 class="card-title">作者 :</h5>
+            <h5 class="card-title">價格 : {{$data->price}}</h5>
+            <a href="#" class="btn btn-primary">購買</a>
         </div>
     </div>
     @endforeach
 </div>
-<a href="{{route('add.Book')}}">新增書籍</a>
-<hr>
-<a href="{{route('home')}}">返回首頁</a>
+<a href="{{route('add.Book')}}">
+    <div id="add-block">
+        <svg xmlns="http://www.w3.org/2000/svg" width="186" height="170.8" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+        </svg>
+    </div>
+</a>
 @endif
 @endsection
